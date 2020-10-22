f8e5088ad7730e2778ad2fd60ee79631b6b50790
prizgitch <57571132+relghub@users.noreply.github.com>
2020-10-22 14:01:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'NzIxNDM0Njg4MzM3NDc3NjMy.XuUeaQ.PhDl_A05DgOVBdY09JEz6vLQ6r4',
+    'token': 'REDACTED',
     'bot': '/nastybot',
     'id': 721434688337477632,
     'prefix': 'n/'
